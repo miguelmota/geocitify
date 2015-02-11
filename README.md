d828c41f90fcbb69710d0589270433a372e38e75
Miguel Mota <miguelmota2@gmail.com>
2015-02-10 20:00:46 -0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# geocitify
+
+[GeoCitify](http://en.wikipedia.org/wiki/GeoCities) any webpage, just like the old days!
+
+# Demo
+
+[http://lab.moogs.io/geocitify](http://lab.moogs.io/geocitify)
+
+# Usage
+
+```javascript
+// Hell yeah, party time
+geocitify();
+
+// With options
+geocitify({
+  interval: 100 // smaller number is faster flashing. default: 100
+});
+
+// Stop the party :(
+geocitify.stop();
+```
+
+# License
+
+MIT
---
+++ geocitify.js
@@ -0,0 +1,40 @@
+(function(root) {
+
+  var interval;
+
+  var geocitify = function(opts) {
+    if (opts && typeof opts !== 'object') {
+      throw new Error('Options must be an object');
+    }
+
+    var t = opts.interval || 100;
+
+    interval = setInterval(function(){
+      [].forEach.call(document.querySelectorAll('*'), function(el, i) {
+        el.style.outline = '2px solid #' + (~~(Math.random()*(1<<24))).toString(16);
+      });
+    }, t);
+  };
+
+  geocitify.stop = function() {
+    window.clearInterval(interval);
+    [].forEach.call(document.querySelectorAll('*'), function(a) {
+      a.style.outline = 'none';
+    });
+  };
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = geocitify;
+    }
+    exports.geocitify = geocitify;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return geocitify;
+    });
+  } else {
+    root.geocitify = geocitify;
+  }
+
+})(this);
+
